ff774859920c18859816217dca1f4b46d6bfec5c
omkarkhopade <omkarkhopade23@gmail.com>
2026-09-16 20:06:06 +0530

    Updated models/courseProgress.js

--- models/courseProgress.js
+++ models/courseProgress.js
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 
 const lectureProgressSchema = new mongoose.Schema({
     lecture: {
+
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Lecture',
         required: [true, 'Lecture reference is required']
+        
     },
     isCompleted: {
         type: Boolean,
